e036e65b28803b93a7605e0f23f74f042ccf03b6
Nazanin Azarian <badi.mojgan@gmail.com>
2023-01-23 18:19:47 +0330

    Merge

--- internal/productmeta/product.go
+++ internal/productmeta/product.go
@@ -5,7 +5,7 @@ import (
 	v1 "github.com/arimanius/digivision-backend/pkg/api/v1"
 )
 
-type Breadcumb struct {
+type Breadcrumb struct {
 	Title string `json:"title"`
 	Url   struct {
 		Uri string `json:"uri"`
@@ -39,14 +39,14 @@ type DigikalaProduct struct {
 	}
 }
 
-func (b Breadcumb) ToCategory(baseUrl string) *v1.Category {
+func (b Breadcrumb) ToCategory(baseUrl string) *v1.Category {
 	return &v1.Category{
 		Title: b.Title,
 		Url:   fmt.Sprintf("%s%s", baseUrl, b.Url.Uri),
 	}
 }
 
-func ToCategories(baseUrl string, breadcrumb []Breadcumb) []*v1.Category {
+func ToCategories(baseUrl string, breadcrumb []Breadcrumb) []*v1.Category {
 	categories := make([]*v1.Category, len(breadcrumb)-1)
 	for i, b := range breadcrumb {
 		if i == len(breadcrumb)-1 {
